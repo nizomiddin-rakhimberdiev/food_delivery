264c978b8fc6ba805c8d1c06a176745d3e5695c8
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2025-01-11 10:09:03 +0500

    teplatelar qo'shildi

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+
+from core.views import home_view, restaurant_view
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('restaurants/<int:id>/', restaurant_view, name='restaurant'),
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+from core.models import FoodCategory, Restaurant
+
+
 # Create your views here.
-def teacher_view(request):
-    pass
+def home_view(request, *args, **kwargs):
+    categories = FoodCategory.objects.all()
+    restaurants = Restaurant.objects.all()
+    context = {
+        'categories': categories,
+        'restaurants': restaurants,
+        'title': 'Home'  # Add title to your template in the base.html file, e.g., <title>{{ title }}</title>
+    }
+    return render(request, 'index.html', context)
+
+
+def restaurant_view(request, id):
+    restaurant = Restaurant.objects.get(id=id)
+    restaurants = Restaurant.objects.all()
+    context = {
+        'restaurant': restaurant,
+        'restaurants': restaurants,  # Include all restaurants for navigation bar in the template
+        'title': restaurant.name  # Add title to your template in the base.html file, e.g., <title>{{ title }}</title>
+    }
+    return render(request, 'shop-grid.html', context)
