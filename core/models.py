a4a8baa1156be7cd73f345367ffee8cdf7607e5e
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2025-01-14 09:49:05 +0500

    add menu

--- core/models.py
+++ core/models.py
@@ -32,6 +32,7 @@ class Food(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     image = models.ImageField(upload_to='food_images/', blank=True, null=True)
     is_available = models.BooleanField(default=True)
 
